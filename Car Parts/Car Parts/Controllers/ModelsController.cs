8556c49e2a4692e787a948f28b587ff43edf533a
Hringald <ednomienaum@gmail.com>
2021-07-16 01:10:46 +0300

    Some Code clean up

---
+++ Car Parts/Car Parts/Controllers/ModelsController.cs
@@ -0,0 +1,41 @@
+namespace Car_Parts.Controllers
+{
+    using Car_Parts.Data;
+    using Car_Parts.Models;
+    using Car_Parts.Models.Parts;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Diagnostics;
+    using System.Linq;
+
+    public class ModelsController : Controller
+    {
+        private readonly CarPartsDbContext data;
+        public ModelsController(CarPartsDbContext data)
+        {
+            this.data = data;
+        }
+
+        public IActionResult Models(string make)
+        {
+            ViewBag.Name = make;
+
+            var modelsModel = this.data.Models.Where(m => m.Make.Name == make)
+                .Select(m => new PartModelViewModel
+                {
+                    Id = m.Id,
+                    Name = m.Name,
+                    ImageUrl = m.ImageUrl
+                }).ToList();
+
+            return this.View(modelsModel);
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Car Parts/Car Parts/Models/Parts/PartViewModel.cs
@@ -0,0 +1,14 @@
+namespace Car_Parts.Models.Parts
+{
+    public class PartViewModel
+    {
+        public string Id { get; init; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string MakeName { get; set; }
+        public string ModelName { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
