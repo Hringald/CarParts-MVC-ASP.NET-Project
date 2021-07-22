b9862bb4ad4d1c54b9c3a256ff9df92dc5386e68
Hringald <ednomienaum@gmail.com>
2021-07-22 11:17:34 +0300

    ..

--- Car Parts/Car Parts/Controllers/ModelsController.cs
+++ Car Parts/Car Parts/Controllers/ModelsController.cs
@@ -1,10 +1,13 @@
 namespace Car_Parts.Controllers
 {
     using Car_Parts.Data;
+    using Car_Parts.Data.Models;
+    using Car_Parts.Infrastructure;
     using Car_Parts.Models;
     using Car_Parts.Models.Parts;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
     using System.Diagnostics;
     using System.Linq;
 
@@ -30,5 +33,80 @@ namespace Car_Parts.Controllers
 
             return this.View(modelsModel);
         }
+
+        //Add Model --------------------------------------------
+        [Authorize]
+        public IActionResult AddModel()
+        {
+            if (!this.UserIsAdmin())
+            {
+                return this.RedirectToAction((nameof(AdminsController.Become)), "Admins");
+            }
+
+            return View(new AddModelFormModel
+            {
+                Makes = this.GetPartModelMakes()
+            });
+        }
+        [HttpPost]
+        [Authorize]
+        public IActionResult AddModel(AddModelFormModel carModel)
+        {
+            var adminId = this.data.Admins
+               .Where(a => a.UserId == this.User.GetId())
+               .Select(d => d.Id)
+               .FirstOrDefault();
+
+            if (string.IsNullOrEmpty(adminId))
+            {
+                return this.RedirectToAction((nameof(AdminsController.Become)), "Admins");
+            }
+
+            if (!this.data.Makes.Any(m => m.Id == carModel.MakeId))
+            {
+                this.ModelState.AddModelError(nameof(carModel.MakeId), "Make is invalid.");
+            }
+
+            var make = this.data.Makes.FirstOrDefault(m => m.Id == carModel.MakeId);
+
+            if (this.data.Models.Where(m => m.Make.Name == make.Name).Any(m => m.Name == carModel.Name))
+            {
+                this.ModelState.AddModelError(nameof(carModel.Name), "Model already exists.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                carModel.Makes = this.GetPartModelMakes();
+
+                return this.View(carModel);
+            }
+
+            var modelModel = new Model
+            {
+                Name = carModel.Name,
+                ImageUrl = carModel.ImageUrl,
+                Make = make,
+                AdminId = adminId
+            };
+
+            make.Models.Add(modelModel);
+            this.data.Models.Add(modelModel);
+            this.data.SaveChanges();
+
+            return this.Redirect("/");
+        }
+        //
+        private ICollection<PartCategoryViewModel> GetPartModelMakes()
+ =>
+     this.data
+         .Makes
+         .Select(p => new PartCategoryViewModel
+         {
+             Id = p.Id,
+             Name = p.Name,
+         }).ToList();
+        //
+        private bool UserIsAdmin() => this.data.Admins
+              .Any(a => a.UserId == this.User.GetId());
     }
 }
